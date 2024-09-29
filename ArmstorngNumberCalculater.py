047ebaf5e378e4f48a4333519a8ccb0714d03d96
oguzkagan1 <oguzkagan1111@gmail.com>
2024-09-29 13:22:03 +0300

    Add commit

--- ArmstorngNumberCalculater.py
+++ ArmstorngNumberCalculater.py
@@ -4,7 +4,7 @@ def is_armstrong_number(number):
        print(digits)
        total_digits = len(digits)
        print(total_digits)
-       armstorn_calculate = sum([digits ** total_digits for digits in digits])
+       armstorn_calculate = sum([digit ** total_digits for digit in digits])
        return armstorn_calculate
 
 number = int(input("Armstorng Number Calculate; "))
